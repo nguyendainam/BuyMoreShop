8bc28ce2b924ebde7a47947582f126c079916854
nguyendainam <nam37012@gmail.com>
2023-12-06 00:14:09 +0700

    failed

--- client/src/components/GetdataCategory.tsx
+++ client/src/components/GetdataCategory.tsx
@@ -1,111 +1,227 @@
-import FormData from 'form-data'
-import { getAllCategory, getAllListCategory, getItemCatById, getAllProductType } from '../services/product'
+import FormData from "form-data";
+import {
+  getAllCategory,
+  getAllListCategory,
+  getItemCatById,
+  getAllProductType,
+} from "../services/product";
 
 interface ListCategoryDropdown {
-  key: string | number
-  title: [object, object]
-  image?: string
+  key: string | number;
+  title: [object, object];
+  image?: string;
 }
 
-const language: string = 'en'
+const language: string = "en";
 
 export const getListCategory = async () => {
-  const response = await getAllCategory()
+  const response = await getAllCategory();
   const dataCategory: [ListCategoryDropdown] = response.data.items.map(
-    item => ({
+    (item) => ({
       key: item.Id,
       titleVi: item.NameVI,
       titleEN: item.NameEN,
-      image: item.ImageCat
+      image: item.ImageCat,
     })
-  )
+  );
 
-  return dataCategory
-}
+  return dataCategory;
+};
 
 interface IdataListCategory {
-  idListCat: string
-  nameENCategory: string
-  nameVICategory: string
-  nameEN: string
-  nameVI: string
-  IdCat: string
+  idListCat: string;
+  nameENCategory: string;
+  nameVICategory: string;
+  nameEN: string;
+  nameVI: string;
+  IdCat: string;
 }
 
 export const AllListCategory = async () => {
-  const response = await getAllListCategory() // Assuming getAllListCategory is a function
-  const resolveData: IdataListCategory[] = response.data.items.map(item => ({
+  const response = await getAllListCategory(); // Assuming getAllListCategory is a function
+  const resolveData: IdataListCategory[] = response.data.items.map((item) => ({
     idListCat: item.idListCat,
     nameENCategory: item.NameVICategory,
     nameVICategory: item.NameENCategory,
     nameEN: item.nameEN,
     nameVI: item.nameVI,
-    IdCat: item.IdCat
-  }))
+    IdCat: item.IdCat,
+  }));
 
-  return resolveData
-}
-
-
+  return resolveData;
+};
 
 interface IItemCat {
-  IdItemCat: string, 
-  nameVI: string,
-  nameEN: string, 
-  IdListCat: string,
-  ListNameEN: string ,
-  ListNameVI: string,
-  CatNameEN: string ,
-  CatNameVI: string,
-  IdCat: number ,
-
-
+  IdItemCat: string;
+  nameVI: string;
+  nameEN: string;
+  imageCat?: string;
+  IdListCat: string;
+  ListNameEN: string;
+  ListNameVI: string;
+  CatNameEN: string;
+  CatNameVI: string;
+  IdCat: number;
 }
 
 export const getAllCategorybyItem = async (key?: string) => {
-
-  let keyId = 'ALL'
+  let keyId = "ALL";
   if (key) {
-    keyId = key
+    keyId = key;
   }
-  const reponse = await getItemCatById(keyId)
-  const resultData:IItemCat = reponse.data.items.map(i => ({
-  IdItemCat: i.IdItemCat, 
-  nameVI: i.nameVI,
-  nameEN: i.nameEN, 
-  IdListCat: i.IdListCat,
-  ListNameEN: i.ListNameEN ,
-  ListNameVI: i.ListNameVI,
-  CatNameEN: i.CatNameEN ,
-  CatNameVI: i.CatNameVI,
-  IdCat: i.IdCat,
-  }))
-  return resultData
+  const reponse = await getItemCatById(keyId);
+  const resultData: IItemCat = reponse.data.items.map((i) => ({
+    IdItemCat: i.IdItemCat,
+    nameVI: i.nameVI,
+    nameEN: i.nameEN,
+    imageCat: i.imageCat,
+    IdListCat: i.IdListCat,
+    ListNameEN: i.ListNameEN,
+    ListNameVI: i.ListNameVI,
+    CatNameEN: i.CatNameEN,
+    CatNameVI: i.CatNameVI,
+    IdCat: i.IdCat,
+  }));
+  return resultData;
+};
+
+interface IListCategory {
+  title: string;
+  key: number;
+  image: string;
+  list: [
+    {
+      key: string;
+      name: string;
+      listItem?: [
+        {
+          keyItem: string;
+          name: string;
+        }
+      ];
+    }
+  ];
 }
 
+interface IItem {
+  keyItem: string;
+  name: string;
+}
 
+interface IListItem {
+  key: string;
+  name: string;
+  listItem?: IItem[];
+}
+
+interface IListCategory {
+  title: string;
+  key: string;
+  image: string;
+  list: IListItem[];
+}
+
+export const ListItemCategoryHome = async (): Promise<IListCategory[]> => {
+  const data = await getAllCategorybyItem();
+  console.log(data);
+
+  const groupedData: { [key: string]: IListCategory } = {};
+
+  Object.values(data).forEach((item) => {
+    const keyCat = item.IdCat;
+    const keyList = item.IdListCat;
+    const keyItem = item.IdItemCat;
+
+    if (keyCat in groupedData) {
+      const existingCat = groupedData[keyCat];
+      const existingList = existingCat.list.find(
+        (list) => list.key === keyList
+      );
+
+      if (existingList) {
+        const existingItem = existingList.listItem?.find(
+          (i) => i.keyItem === keyItem
+        );
+
+        if (existingItem) {
+          // Item already exists, you can handle this case if needed
+        } else {
+          // Add the item to an existing list
+          existingList.listItem.push({
+            keyItem: keyItem,
+            name: item.nameVI,
+          });
+        }
+      } else {
+        // Create a new list
+        existingCat.list.push({
+          key: keyList,
+          name: item.ListNameVI,
+          listItem: [
+            {
+              keyItem: keyItem,
+              name: item.nameVI,
+            },
+          ],
+        });
+      }
+    } else {
+      // Create a new category
+      groupedData[keyCat] = {
+        title: item.CatNameVI,
+        key: keyCat,
+        image: item.imageCat,
+        list: [
+          {
+            key: keyList,
+            name: item.ListNameVI,
+            listItem: [
+              {
+                keyItem: keyItem,
+                name: item.nameVI,
+              },
+            ],
+          },
+        ],
+      };
+    }
+  });
+
+  const resultMenu: IListCategory[] = Object.values(groupedData);
+
+  return resultMenu;
+};
+
+// export const ListItemCategoryHome = async () => {
+//   const data = await getAllCategorybyItem();
+//   // const groupedData: { [key: string]: IListCategory } = {};
+//   // Object.values(data).forEach((item) => {
+//   //   const key = item.IdItemCat;
+
+//   // });
+
+//   // const resultMenu: IListCategory[] = Object.values(groupedData);
+
+//   return data;
+// };
 
 interface ITypeProduct {
-  Id: string
-  nameVI: string,
-  nameEN: string,
-  Image?: string,
-  isShow: boolean
+  Id: string;
+  nameVI: string;
+  nameEN: string;
+  Image?: string;
+  isShow: boolean;
 }
 
 export const AllProductType = async () => {
+  const getdata = await getAllProductType();
+  const resultData: ITypeProduct[] = getdata.data.items.map((i) => ({
+    Id: i.Id,
+    nameVI: i.NameVI,
+    nameEN: i.NameEN,
+    Image: i.Image,
+    isShow: i.IsShow,
+  }));
 
-  const getdata = await getAllProductType()
-  const resultData: ITypeProduct[] = getdata.data.items.map(i => (
-    {
-      Id: i.Id,
-      nameVI: i.NameVI,
-      nameEN: i.NameEN,
-      Image: i.Image,
-      isShow: i.IsShow
-
-    }
-  ))
-
-  return resultData
-}
+  return resultData;
+};
--- client/src/container/Clients/Header/Header.tsx
+++ client/src/container/Clients/Header/Header.tsx
@@ -1,120 +1,130 @@
-import React, { useState, useEffect } from 'react'
-import style from './Header.module.scss'
-import Language from '../../../components/language'
-import { Logo } from '../../../assets/image'
-import Search from '../../../components/search'
-import _throttle from 'lodash/throttle'
-import { BiUser, BiSolidCartAlt } from 'react-icons/bi'
-import { AiOutlineMenu } from 'react-icons/ai'
-import { Dropdown, Menu, Space } from 'antd'
-import { DownOutlined } from '@ant-design/icons'
-import dataTest from '../../../components/datatest/listItem.json'
-import category from '../../../components/datatest/category.json'
-import type { MenuProps } from 'antd'
-import HeaderReponsive from './HeaderReponsive'
+import React, { useState, useEffect } from "react";
+import style from "./Header.module.scss";
+import Language from "../../../components/language";
+import { Logo } from "../../../assets/image";
+import Search from "../../../components/search";
+import _throttle from "lodash/throttle";
+import { BiUser, BiSolidCartAlt } from "react-icons/bi";
+import { AiOutlineMenu } from "react-icons/ai";
+import { Dropdown, Menu, Space } from "antd";
+import { DownOutlined } from "@ant-design/icons";
+import dataTest from "../../../components/datatest/listItem.json";
+import category from "../../../components/datatest/category.json";
+import type { MenuProps } from "antd";
+import HeaderReponsive from "./HeaderReponsive";
+import { ListItemCategoryHome } from "../../../components/GetdataCategory";
 
-type MenuItem = Required<MenuProps>['items'][number]
+type MenuItem = Required<MenuProps>["items"][number];
 
 function getItem(
   label: React.ReactNode,
   key?: React.Key | null,
   icon?: React.ReactNode,
   children?: MenuItem[],
-  type?: 'group'
+  type?: "group"
 ): MenuItem {
   return {
     key,
     icon,
     children,
     label,
-    type
-  } as MenuItem
+    type,
+  } as MenuItem;
 }
 
 export default function Header() {
-  const [isScrolled, setIsScrolled] = useState(false)
+  const [isScrolled, setIsScrolled] = useState(false);
 
   const listItems = [
-    { label: 'Home', key: 'Home', option: true },
-    { label: 'Shop', key: 'Shop', option: true },
-    { label: 'Product', key: 'Product', option: true },
-    { label: 'Pages', key: 'Pages', option: true },
-    { label: 'Blog', key: 'Blog', option: true },
-    { label: 'Contact Us', key: 'home', option: false }
-  ]
+    { label: "Home", key: "Home", option: true },
+    { label: "Shop", key: "Shop", option: true },
+    { label: "Product", key: "Product", option: true },
+    { label: "Pages", key: "Pages", option: true },
+    { label: "Blog", key: "Blog", option: true },
+    { label: "Contact Us", key: "home", option: false },
+  ];
+
+  const handleGetMenuCate = async () => {
+    const menu = await ListItemCategoryHome();
+    console.log("MEEEEEEEEEEENYYYYYYYYYYY", menu);
+  };
+
+  useEffect(() => {
+    handleGetMenuCate();
+  });
 
   useEffect(() => {
     const handleScroll = _throttle(() => {
-      const scrollThreshold = 100
-      const scrollPosition = Math.round(window.scrollY)
+      const scrollThreshold = 100;
+      const scrollPosition = Math.round(window.scrollY);
       requestAnimationFrame(() => {
-        setIsScrolled(scrollPosition > scrollThreshold)
-      })
-    }, 100)
+        setIsScrolled(scrollPosition > scrollThreshold);
+      });
+    }, 100);
 
-    window.addEventListener('scroll', handleScroll)
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
-  })
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
 
   const handleGetData = (key: string) => {
     if (dataTest[key]) {
-      const menuItems = dataTest[key].map(item => (
+      const menuItems = dataTest[key].map((item) => (
         <Menu.Item key={item.key} className={style.itemMenu}>
           <div className={style.formDropdownItem}> {item.label}</div>
         </Menu.Item>
-      ))
+      ));
 
-      return <Menu className={style.formDropdownMenu}>{menuItems}</Menu>
+      return <Menu className={style.formDropdownMenu}>{menuItems}</Menu>;
     } else {
-      return <></>
+      return <></>;
     }
-  }
+  };
 
-  const menuComponents = listItems.map(item => {
-    const menu = handleGetData(item.key)
+  const menuComponents = listItems.map((item) => {
+    const menu = handleGetData(item.key);
     return (
       <Dropdown
         key={item.key}
         className={style.itemDropdown}
         overlay={menu}
-      // onClick={() => console.log(item.key)}
+        // onClick={() => console.log(item.key)}
       >
         <Space>
           {item.label}
-          {item.option ? <DownOutlined /> : ''}
+          {item.option ? <DownOutlined /> : ""}
         </Space>
       </Dropdown>
-    )
-  })
+    );
+  });
 
   const OptionCategory = () => {
-    const testData: MenuItem[] = category.map(item => {
+    const testData: MenuItem[] = category.map((item) => {
       const subItems: MenuItem[] | undefined =
         item.list.length > 0
-          ? item.list.map(itemChild => getItem(itemChild.name, itemChild.key))
-          : undefined
+          ? item.list.map((itemChild) => getItem(itemChild.name, itemChild.key))
+          : undefined;
 
-      return getItem(item.title, item.key, null, subItems)
-    })
+      return getItem(item.title, item.key, null, subItems);
+    });
 
-    const onClick: MenuProps['onClick'] = e => {
-      console.log('click', e)
-    }
+    const onClick: MenuProps["onClick"] = (e) => {
+      console.log("click", e);
+    };
     const categoryMenuItems = (
       <Menu
         onClick={onClick}
         style={{ width: 256 }}
-        mode='vertical'
+        mode="vertical"
         items={testData}
       />
-    )
+    );
 
     return (
       <Dropdown
-        trigger={['click']}
+        trigger={["click"]}
         className={style.dropdownCategory}
         overlay={categoryMenuItems}
       >
@@ -127,11 +137,11 @@ export default function Header() {
           </div>
         </div>
       </Dropdown>
-    )
-  }
+    );
+  };
 
   return (
-    <div className={`${style.mainHeader} ${isScrolled ? style.scrolled : ''}`}>
+    <div className={`${style.mainHeader} ${isScrolled ? style.scrolled : ""}`}>
       <div className={style.topHeader}>
         <div>You are a student and students get 20% discount</div>
         <div>
@@ -159,7 +169,7 @@ export default function Header() {
             </div>
             <div className={style.centerForm}>
               <div className={style.fsmin}>Login</div>
-              <div className='m-2'>Account</div>
+              <div className="m-2">Account</div>
             </div>
           </div>
           <div className={style.iconCart}>
@@ -167,12 +177,12 @@ export default function Header() {
           </div>
         </div>
       </div>
-      <div className={`${style.MenuHeader} ${isScrolled ? style.active : ''}`}>
+      <div className={`${style.MenuHeader} ${isScrolled ? style.active : ""}`}>
         <OptionCategory />
         <div className={style.centerMenuHeader}>
           <div className={style.centerDropdown}>{menuComponents}</div>
         </div>
       </div>
     </div>
-  )
+  );
 }
